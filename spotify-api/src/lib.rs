6f34bc118319cee47792697bfa228890850842a1
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2025-08-01 21:53:52 +0200

    bunch of stuff

--- spotify-api/src/lib.rs
+++ spotify-api/src/lib.rs
@@ -1,10 +1,15 @@
+use anyhow::Context;
 use base64::Engine;
+use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
-use reqwest::{Client, Request};
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use reqwest_retry::RetryTransientMiddleware;
+use reqwest_retry::policies::ExponentialBackoff;
+pub use rspotify::model::PlaylistId;
 use rspotify::{
   AuthCodeSpotify, Config, Credentials, OAuth,
   clients::OAuthClient,
-  model::{FullTrack, Id, Page, PlayableItem, PlaylistId, PlaylistItem, SavedTrack},
+  model::{Id, Page, PlayableItem, PlaylistItem, SavedTrack},
   prelude::BaseClient,
   scopes,
 };
@@ -12,7 +17,7 @@ use rspotify::{
 use tokio::io::AsyncWriteExt;
 
 #[allow(clippy::missing_panics_doc)]
-pub async fn driver() -> anyhow::Result<()> {
+pub async fn authenticate() -> anyhow::Result<AuthCodeSpotify> {
   let creds = Credentials::from_env().expect("Credentials missing");
   let oauth = OAuth::from_env(scopes!("user-library-read")).expect("Cant init oauth");
   let config = Config {
@@ -23,17 +28,23 @@ pub async fn driver() -> anyhow::Result<()> {
 
   let url = spotify.get_authorize_url(false)?;
   spotify.prompt_for_token(&url).await?;
-
-  let client = Client::new();
-  get_from_liked(spotify, client).await?;
-
-  Ok(())
+  Ok(spotify)
 }
 
-async fn download_image(song: Song, client: Client) -> anyhow::Result<()> {
+async fn download_image(
+  song: Song,
+  client: ClientWithMiddleware,
+  bar: ProgressBar,
+) -> anyhow::Result<()> {
   let encoded = base64::engine::general_purpose::URL_SAFE.encode(song.name.as_bytes());
 
   let output_path = "./images/".to_owned() + &encoded + "@" + &song.id + ".jpg";
+  // TODO: Some songs seem to not have images at all, skipping them for now, see if I want to do
+  // something else about them at some point
+  if song.images.is_empty() {
+    return Ok(());
+  }
+
   // TODO: If image sizes are always ordered, maybe consider using the smaller one?
   let response = client.get(&song.images[0]).send().await?;
   let image_data = response.bytes().await?;
@@ -42,51 +53,77 @@ async fn download_image(song: Song, client: Client) -> anyhow::Result<()> {
   let mut file = tokio::fs::File::create(&output_path).await?;
   file.write_all(&image_data).await?;
 
+  bar.inc(1);
+
   Ok(())
 }
 
-fn create_bar() -> ProgressBar {
-  let bar = ProgressBar::new_spinner();
-  bar.set_style(
+fn create_bar() -> (ProgressBar, ProgressBar) {
+  let multi_bar = indicatif::MultiProgress::new();
+  let bar1 = ProgressBar::new_spinner();
+  bar1.set_style(
     ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
       .expect("Invalid template")
       .progress_chars("##-"),
   );
-  bar
+  let bar2 = ProgressBar::new_spinner();
+  bar2.set_style(
+    ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+      .expect("Invalid template")
+      .progress_chars("##-"),
+  );
+
+  (multi_bar.add(bar1), multi_bar.add(bar2))
 }
 
 async fn process_items<F, Fut, Item>(
-  client: Client,
   fetch_page: F,
   convert: impl Fn(Item) -> anyhow::Result<Song>,
+  list_name: String,
 ) -> anyhow::Result<()>
 where
   F: Fn(u32, u32) -> Fut,
   Fut: Future<Output = anyhow::Result<Page<Item>>>,
   Item: for<'de> serde::de::Deserialize<'de>,
 {
+  let retry_policy = ExponentialBackoff::builder().build_with_max_retries(3);
+  let client = ClientBuilder::new(
+    reqwest::ClientBuilder::new()
+      .timeout(std::time::Duration::from_secs(10))
+      .build()?,
+  )
+  .with(RetryTransientMiddleware::new_with_policy(retry_policy))
+  .build();
+  // let client = Client::new();
+
   let mut handles = Vec::new();
-  let bar = create_bar();
+
+  let (bar1, bar2) = create_bar();
+  bar1.set_message(format!("Fetching {list_name}..."));
+  bar2.set_message("Downloading thumbnails...");
+
   let limit = 50;
   let mut offset = 0;
 
   loop {
-    let page = fetch_page(limit, offset).await?;
+    let page = fetch_page(limit, offset)
+      .await
+      .context("Failed to fetch page")?;
 
-    bar.set_length(u64::from(page.total));
-    bar.set_message("Downloading song thumbnails");
+    bar1.set_length(u64::from(page.total));
+    bar2.set_length(u64::from(page.total));
 
     for item in page.items {
       match convert(item) {
         Ok(song) => {
-          let task = tokio::spawn(download_image(song, client.clone()));
+          let task = tokio::spawn(download_image(song, client.clone(), bar2.clone()));
           handles.push(task);
         }
         Err(e) => {
-          eprintln!("Skipping item: {}", e);
+          eprintln!("Skipping item: {e}");
         }
       }
-      bar.inc(1);
+      bar1.inc(1);
     }
 
     if page.next.is_none() {
@@ -95,38 +132,60 @@ where
     offset += limit;
   }
 
-  bar.finish_with_message("Done");
-
-  let bar = create_bar();
-  bar.set_length(handles.len().try_into()?);
-  bar.set_message("Joining background tasks");
+  bar1.finish_with_message(format!("Done fetching {list_name}"));
 
   for task in handles {
     if let Err(e) = task.await? {
       eprintln!("Task error: {e}");
     }
-    bar.inc(1);
   }
 
-  bar.finish_with_message("Done");
+  bar2.finish_with_message("Thumbnails downloaded");
   Ok(())
 }
 
-async fn get_from_playlist(spotify: AuthCodeSpotify, client: Client) -> anyhow::Result<()> {
-  // TODO: Don't hardcode the id
-  let id = PlaylistId::from_id("22nJr6nQ1OSEXKwipYOZ3j")?;
+pub async fn get_from_all_playlists(spotify: AuthCodeSpotify) -> anyhow::Result<()> {
+  let mut playlists = spotify.current_user_playlists();
+
+  while let Some(playlist) = playlists.next().await {
+    let id = playlist?.id;
+    get_from_playlist(spotify.clone(), id).await?;
+  }
+
+  Ok(())
+}
+
+pub async fn get_from_playlist(
+  spotify: AuthCodeSpotify,
+  playlist_id: PlaylistId<'_>,
+) -> anyhow::Result<()> {
   let spotify_clone = spotify.clone();
 
+  let playlist = spotify.playlist(playlist_id.clone(), None, None).await;
+  // I allow this here to be easier to find if it does go wrong
+  if playlist.is_err() {
+    eprintln!(
+      "Encountered error for playlist id {}, skipping...",
+      &playlist_id
+    );
+
+    return Ok(());
+  }
+
+  let playlist = playlist?;
+  let playlist_name = format!("\"{}\"", playlist.name);
+
   process_items(
-    client,
     move |limit, offset| {
       let spotify = spotify_clone.clone();
-      let id = id.clone();
+      let id = playlist_id.clone();
       async move {
         spotify
           .playlist_items_manual(
             id,
-            Some("items(track.id,track.name,track.album.images)"),
+            // I wanted to use Some("items(track.id,track.name,track.album.images)")
+            // but the library's json parsing doesn't like that.
+            None,
             None,
             Some(limit),
             Some(offset),
@@ -136,15 +195,15 @@ async fn get_from_playlist(spotify: AuthCodeSpotify, client: Client) -> anyhow::
       }
     },
     Song::try_from,
+    playlist_name,
   )
   .await
 }
 
-async fn get_from_liked(spotify: AuthCodeSpotify, client: Client) -> anyhow::Result<()> {
+pub async fn get_from_liked(spotify: AuthCodeSpotify) -> anyhow::Result<()> {
   let spotify_clone = spotify.clone();
 
   process_items(
-    client,
     move |limit, offset| {
       let spotify = spotify_clone.clone();
       async move {
@@ -155,6 +214,7 @@ async fn get_from_liked(spotify: AuthCodeSpotify, client: Client) -> anyhow::Res
       }
     },
     |item| Ok(Song::from(item)),
+    "liked/saved songs".to_owned(),
   )
   .await
 }
@@ -189,7 +249,12 @@ impl TryFrom<PlaylistItem> for Song {
         .collect::<Vec<_>>();
 
       // FIXME: If its local, id wont exist
-      let id = item.id.unwrap().id().to_owned();
+      let id = item
+        .id
+        .context("Local songs are not handled yet.")
+        .unwrap()
+        .id()
+        .to_owned();
 
       // bar.set_message("Fetching ".to_owned() + &name);
       Ok(Self {
@@ -226,7 +291,13 @@ impl From<SavedTrack> for Song {
       .collect::<Vec<_>>();
 
     // FIXME: If its local, id wont exist
-    let id = item.track.id.unwrap().id().to_owned();
+    let id = item
+      .track
+      .id
+      .context("Local songs are not handled yet.")
+      .unwrap()
+      .id()
+      .to_owned();
 
     // bar.set_message("Fetching ".to_owned() + &name);
     Self {
