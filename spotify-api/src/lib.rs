f7fa074ef4f3becaf944ad54b09eb916911c0307
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2025-08-01 15:46:41 +0200

    workspaces

---
+++ spotify-api/src/lib.rs
@@ -0,0 +1,246 @@
+use base64::Engine;
+use indicatif::{ProgressBar, ProgressStyle};
+use reqwest::{Client, Request};
+use rspotify::{
+  AuthCodeSpotify, Config, Credentials, OAuth,
+  clients::OAuthClient,
+  model::{FullTrack, Id, Page, PlayableItem, PlaylistId, PlaylistItem, SavedTrack},
+  prelude::BaseClient,
+  scopes,
+};
+
+use tokio::io::AsyncWriteExt;
+
+#[allow(clippy::missing_panics_doc)]
+pub async fn driver() -> anyhow::Result<()> {
+  let creds = Credentials::from_env().expect("Credentials missing");
+  let oauth = OAuth::from_env(scopes!("user-library-read")).expect("Cant init oauth");
+  let config = Config {
+    token_cached: true,
+    ..Default::default()
+  };
+  let spotify = AuthCodeSpotify::with_config(creds, oauth, config);
+
+  let url = spotify.get_authorize_url(false)?;
+  spotify.prompt_for_token(&url).await?;
+
+  let client = Client::new();
+  get_from_liked(spotify, client).await?;
+
+  Ok(())
+}
+
+async fn download_image(song: Song, client: Client) -> anyhow::Result<()> {
+  let encoded = base64::engine::general_purpose::URL_SAFE.encode(song.name.as_bytes());
+
+  let output_path = "./images/".to_owned() + &encoded + "@" + &song.id + ".jpg";
+  // TODO: If image sizes are always ordered, maybe consider using the smaller one?
+  let response = client.get(&song.images[0]).send().await?;
+  let image_data = response.bytes().await?;
+
+  tokio::fs::create_dir_all("./images/").await?;
+  let mut file = tokio::fs::File::create(&output_path).await?;
+  file.write_all(&image_data).await?;
+
+  Ok(())
+}
+
+fn create_bar() -> ProgressBar {
+  let bar = ProgressBar::new_spinner();
+  bar.set_style(
+    ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+      .expect("Invalid template")
+      .progress_chars("##-"),
+  );
+  bar
+}
+
+async fn get_from_playlist(spotify: AuthCodeSpotify, client: Client) -> anyhow::Result<()> {
+  let mut handles = Vec::new();
+
+  let bar = create_bar();
+
+  let limit = 50;
+  let mut offset = 0;
+
+  let id = PlaylistId::from_id("22nJr6nQ1OSEXKwipYOZ3j")?;
+
+  loop {
+    let page = spotify
+      .playlist_items_manual(
+        id.clone(),
+        Some("items(track.id,track.name,track.album.images)"),
+        None,
+        Some(limit),
+        Some(offset),
+      )
+      .await?;
+
+    bar.set_length(u64::from(page.total));
+    bar.set_message("Downloading song thumbnails");
+
+    for item in page.items {
+      if let Ok(song) = Song::try_from(item) {
+        let handle = tokio::spawn(download_image(song, client.clone()));
+        handles.push(handle);
+      }
+
+      bar.inc(1);
+    }
+
+    // The iteration ends when the `next` field is `None`. Otherwise, the
+    // Spotify API will keep returning empty lists from then on.
+    if page.next.is_none() {
+      break;
+    }
+
+    offset += limit;
+  }
+
+  bar.finish_with_message("Done");
+
+  let bar = create_bar();
+  bar.set_length(handles.len().try_into().unwrap());
+  bar.set_message("Joining background tasks");
+
+  for task in handles {
+    // task.await?;
+    if let Err(e) = task.await? {
+      dbg!(e);
+    }
+    bar.inc(1);
+  }
+  bar.finish_with_message("Done");
+
+  Ok(())
+}
+
+async fn get_from_liked(spotify: AuthCodeSpotify, client: Client) -> anyhow::Result<()> {
+  let mut handles = Vec::new();
+
+  let bar = create_bar();
+
+  let limit = 50;
+  let mut offset = 0;
+
+  loop {
+    let page = spotify
+      .current_user_saved_tracks_manual(None, Some(limit), Some(offset))
+      .await?;
+
+    bar.set_length(u64::from(page.total));
+    bar.set_message("Downloading song thumbnails");
+
+    for item in page.items {
+      let song = Song::from(item);
+
+      let handle = tokio::spawn(download_image(song, client.clone()));
+      handles.push(handle);
+      // download_image(song).await?;
+    }
+
+    // The iteration ends when the `next` field is `None`. Otherwise, the
+    // Spotify API will keep returning empty lists from then on.
+    if page.next.is_none() {
+      break;
+    }
+
+    offset += limit;
+  }
+
+  bar.finish_with_message("Done");
+
+  let bar = create_bar();
+  bar.set_length(handles.len().try_into().unwrap());
+  bar.set_message("Joining background tasks");
+
+  for task in handles {
+    // task.await?;
+    if let Err(e) = task.await? {
+      dbg!(e);
+    }
+    bar.inc(1);
+  }
+  bar.finish_with_message("Done");
+
+  Ok(())
+}
+
+#[allow(unused)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct Song {
+  id: String,
+  name: String,
+  artists: Vec<String>,
+  images: Vec<String>,
+}
+
+impl TryFrom<PlaylistItem> for Song {
+  type Error = anyhow::Error;
+
+  fn try_from(value: PlaylistItem) -> Result<Self, Self::Error> {
+    if let Some(PlayableItem::Track(item)) = value.track {
+      let name = item.name;
+
+      let artists = item
+        .artists
+        .into_iter()
+        .map(|el| el.name)
+        .collect::<Vec<_>>();
+
+      let images = item
+        .album
+        .images
+        .into_iter()
+        .map(|el| el.url)
+        .collect::<Vec<_>>();
+
+      // FIXME: If its local, id wont exist
+      let id = item.id.unwrap().id().to_owned();
+
+      // bar.set_message("Fetching ".to_owned() + &name);
+      Ok(Self {
+        id,
+        name,
+        artists,
+        images,
+      })
+    } else {
+      Err(anyhow::anyhow!("Could not convert {:?}", value.track))
+    }
+  }
+}
+
+// One unwrap used
+#[allow(clippy::fallible_impl_from)]
+impl From<SavedTrack> for Song {
+  fn from(item: SavedTrack) -> Self {
+    let name = item.track.name;
+
+    let artists = item
+      .track
+      .artists
+      .into_iter()
+      .map(|el| el.name)
+      .collect::<Vec<_>>();
+
+    let images = item
+      .track
+      .album
+      .images
+      .into_iter()
+      .map(|el| el.url)
+      .collect::<Vec<_>>();
+
+    // FIXME: If its local, id wont exist
+    let id = item.track.id.unwrap().id().to_owned();
+
+    // bar.set_message("Fetching ".to_owned() + &name);
+    Self {
+      id,
+      name,
+      artists,
+      images,
+    }
+  }
+}
